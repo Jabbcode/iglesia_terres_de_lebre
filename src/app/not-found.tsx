38c4704d0dce89f68ccb280c183acd9d73aeb45e
Jabbcode <jabbsontouch@gmail.com>
2025-01-25 23:17:32 +0100

    feat: Modificando estilos de not-found + add component badge

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,14 @@
 export default function NotFound() {
   return (
     <section className="flex justify-center items-center h-[100vh]">
-      404 - Pagina no encontrada
+      <h1 className="inline-block mt-0 mr-[20px] ml-0 mb-0 pt-0 pr-[23px] pl-0 pb-0 text-2xl font-medium align-top leading-[49px] tracking-widest border-r-2">
+        404
+      </h1>
+      <div>
+        <h2 className=" text-sm font-normal leading-[49px] m-0">
+          Pagina no encontrada
+        </h2>
+      </div>
     </section>
   );
 }
